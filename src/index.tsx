6b0afe2995570a1245b6a94c9d0c27620147109e
Jesse B. Hannah <jesse@jbhannah.net>
2019-03-26 16:44:05 -0700

    React

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-const app = document.createElement("div")
-app.id = "app"
-app.innerHTML = "Hello, Webpack!"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
 
+const app = document.createElement("div")
 document.body.prepend(app)
+
+ReactDOM.render(<p>Hello, React!</p>, app)
