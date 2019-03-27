3c623da5fad15d4f3ec5c67d1bd864ef390093a0
Jesse B. Hannah <jesse@jbhannah.net>
2019-03-27 15:22:49 -0700

    Apollo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,21 @@
+import ApolloClient from "apollo-boost"
 import * as React from "react"
+import { ApolloProvider } from "react-apollo"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { Dashboard } from "./Dashboard"
+
+const client = new ApolloClient({
+  uri: GRAPHQL_ENDPOINT,
+})
 
 export class App extends React.Component {
   public render() {
     return (
-      <Router>
-        <Route path="/" exact component={Dashboard} />
-      </Router>
+      <ApolloProvider {...{ client }}>
+        <Router>
+          <Route path="/" exact component={Dashboard} />
+        </Router>
+      </ApolloProvider>
     )
   }
 }
