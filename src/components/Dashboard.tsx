c4273d161d80dfadf3bf837628cf2235bb474c22
Jesse B. Hannah <jesse@jbhannah.net>
2019-04-07 10:57:56 -0700

    TodoList component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import { LogoutUser } from "./LogoutUser"
+import { TodoList } from "./TodoList"
 
 export class Dashboard extends React.Component {
   public render() {
     return (
       <div>
         <h2>Dashboard</h2>
+        <TodoList />
         <LogoutUser />
       </div>
     )
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,43 @@
+import gql from "graphql-tag"
+import * as React from "react"
+import { Query } from "react-apollo"
+import { SessionContext } from "./SessionContext"
+
+export const GET_TODOS = gql`
+  query {
+    getTodos {
+      id
+      text
+      createdAt
+      completedAt
+    }
+  }
+`
+
+export class TodoList extends React.Component {
+  public static contextType = SessionContext
+  public context!: React.ContextType<typeof SessionContext>
+
+  public render() {
+    return (
+      <Query query={GET_TODOS}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return "Loading…"
+          }
+          if (error) {
+            return `Error! ${error.message}`
+          }
+
+          return (
+            <ul>
+              {data.getTodos.map(t => (
+                <li key={t.id}>{t.text}</li>
+              ))}
+            </ul>
+          )
+        }}
+      </Query>
+    )
+  }
+}
