09c0675dc154a90316063a4dc41219000aa288a1
Jesse B. Hannah <jesse@jbhannah.net>
2019-04-10 17:00:14 -0700

    Add checkbox to toggle todo done state

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -9,8 +9,37 @@ export interface TodoListItemProps extends UpdateTodoProps {
 export class TodoListItem extends React.Component<TodoListItemProps> {
   public render() {
     const {
-      todo: { text },
+      todo: { completedAt, text },
     } = this.props
-    return <li>{text}</li>
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={completedAt !== null}
+          onChange={this.handleChecked}
+        />
+        {text}
+      </li>
+    )
+  }
+
+  private handleChecked = () => {
+    const {
+      updateTodo,
+      todo: { completedAt, createdAt, id, text },
+    } = this.props
+    updateTodo({
+      optimisticResponse: {
+        __typename: "Mutation",
+        updateTodo: {
+          __typename: "Todo",
+          completedAt: completedAt === null ? new Date() : null,
+          createdAt,
+          id,
+          text,
+        },
+      },
+      variables: { done: completedAt === null, id, text },
+    })
   }
 }
